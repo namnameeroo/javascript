d201e192507ea23dac3192565ce055c23bdbc935
Namhee KIM <69442847+namnameeroo@users.noreply.github.com>
2022-07-30 11:46:06 +0900

    Update node-input.js

--- beginner/node-input.js
+++ beginner/node-input.js
@@ -1,11 +1,12 @@
 const readline = require("readline");
 const rl = readline.createInterface({
-  input : process.stdin,
-  output : process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-rl.on("line", (response)=>
-       console.log(response)  // 사용자 입력값 변수 : response
-).on("close", ()=>
+rl.on("line", (response) => {
+  console.log(response); // 사용자 입력값 변수 : response
+  rl.close();
+}).on("close", () => {
   process.exit();
-)
+});
